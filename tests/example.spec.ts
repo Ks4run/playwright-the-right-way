fac76933fbfa44419d08752a0051a91d20623aca
Sarun Kaewwongsa <k.sarun@odds.team>
2025-12-02 21:22:18 +0700

    install lib pritter & formatter

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,31 +1,34 @@
-import { test } from '@playwright/test';
-import { LoginPage } from '../pages/login-page';
-import { StorePage } from '../pages/store-page';
-import { MailPit } from '../pages/mailpit';
+import { test } from '@playwright/test'
+import { LoginPage } from '../pages/login-page'
+import { StorePage } from '../pages/store-page'
+import { MailPit } from '../pages/mailpit'
 
 test.describe('ODT Merchandise', () => {
+    test('Buy merchandise success', async ({ page }) => {
+        const loginPage = new LoginPage(page)
+        const storePage = new StorePage(page)
+        const mailPit = new MailPit(page)
 
-  test('Buy merchandise success', async ({ page }) => {
+        await loginPage.goToWebsite()
+        await loginPage.userNameAndPasswordField('customer1', 'password')
+        await loginPage.clickLoginButton()
 
-    const loginPage = new LoginPage(page);
-    const storePage = new StorePage(page);
-    const mailPit = new MailPit(page);
+        await storePage.selectProductAddToCartNumberOfList(0)
+        await storePage.selectProductAddToCartNumberOfList(1)
+        await storePage.openShoppingCart()
+        await storePage.checkoutProductInCart()
 
-    await loginPage.goToWebsite();
-    await loginPage.userNameAndPasswordField('customer1', 'password');
-    await loginPage.clickLoginButton();
+        await storePage.shippingAddressInfo(
+            'kendo',
+            'koden',
+            'kendo-email',
+            '99999',
+        )
+        await storePage.clickConfirmPaymentButton()
 
-    await storePage.selectProductAddToCartNumberOfList(0);
-    await storePage.selectProductAddToCartNumberOfList(1);
-    await storePage.openShoppingCart();
-    await storePage.checkoutProductInCart();
-    
-    await storePage.shippingAddressInfo('kendo', 'koden', 'kendo-email', '99999');
-    await storePage.clickConfirmPaymentButton();
+        await storePage.verifyThankyouWording()
 
-    await storePage.verifyThankyouWording();
-
-    await mailPit.goToMailPit();
-    await mailPit.verifyReceivedMailSuccess(/^Hi Kendo Koden/);
-  });
-});
+        await mailPit.goToMailPit()
+        await mailPit.verifyReceivedMailSuccess(/^Hi Kendo Koden/)
+    })
+})
